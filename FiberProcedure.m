3ee1fd17639f1f41843147bf93e2006ababe0604
shmp0722 <shmp0722@gmail.com>
2015-05-14 22:32:33 +0900

    Procedure

---
+++ FiberProcedure.m
@@ -0,0 +1,18 @@
+function FiberProcedure
+
+%% Make sure your subject's conTrack directory
+% is appropriately formatted in at least these three ways.
+
+%
+%% ctrInit
+%  Generate 5000 fibers from OC to LGN which is optic tract
+%  LGN to V1 100000 fibers which is optic radiation
+%
+
+
+%% next step; using AFQ_removeFiberOutliers reduce fiber numbers 
+% by Max length (4) and Max distance (4),
+
+% if you want to check how tehse fibers look like, use these two 
+% quench (cmd line) or AFQ_RenderFibers (on matlab)
+
