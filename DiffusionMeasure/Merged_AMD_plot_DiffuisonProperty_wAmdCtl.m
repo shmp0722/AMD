1d003948ed6946981953f57e643da25722c6c22f
shmp0722 <shmp0722@gmail.com>
2016-08-29 22:53:20 +0900

    Figures

--- DiffusionMeasure/Merged_AMD_plot_DiffuisonProperty_wAmdCtl.m
+++ DiffusionMeasure/Merged_AMD_plot_DiffuisonProperty_wAmdCtl.m
@@ -78,31 +78,12 @@ for subID = 1:length(ACH);
 end
 
 %% FA
-
-% %%
-% vals = 'fa';
-% switch vals
-%     case 'fa'
-%         val_AC = fa(AMD_Ctl,:);
-%         val_AMD = fa(AMD,:);
-%     case 'md'
-%         val_AC = md(AMD_Ctl,:);
-%         val_AMD = md(AMD,:);
-%         
-%     case 'ad'
-%         val_AC = ad(AMD_Ctl,:);
-%         val_AMD = ad(AMD,:);
-%         
-%     case 'rd'
-%         val_AC = rd(AMD_Ctl,:);
-%         val_AMD = rd(AMD,:);
-% end
 val_AC = fa(AMD_Ctl,:);
 val_AMD = fa(AMD,:);
 AMD_data  = val_AMD;
+vals ='fa';
 
-%% Wilcoxon Single rank test
-% container
+% Wilcoxon Single rank test
 group =2;
 M = length(AMD_Ctl);
 pac = nan(M,group);
@@ -113,6 +94,7 @@ for jj= 1: nodes
     pac(1:8,2)= val_AMD(:,jj);
     
     [p(jj),h(jj),~] = signrank(pac(:,1),pac(:,2));
+            [P(jj),H(jj),~] =          ttest(pac(:,1),pac(:,2));
     %     co = multcompare(stats(jj),'display','off');
     %     C{jj}=co;
 end
@@ -184,8 +166,9 @@ if ~isempty(SavePath)
     %     saveas(G,fullfile(SavePath, [vals,'_',T.String]),'bmp')
 end
 
-%% OT AD
+%% AD
 %
+vals ='ad';
 val_AC = ad(AMD_Ctl,:);
 val_AMD = ad(AMD,:);
 
@@ -196,6 +179,8 @@ AMD_data  = val_AMD;
 group =2;
 M = length(AMD_Ctl);
 pac = nan(M,group);
+
+clear h
 
 for jj= 1: nodes
     
@@ -251,18 +236,18 @@ xlabel('Location')
 %     case {}
 switch fibID
     case {1}
-        b=[0.1,0.8];
+        b=[0.8, 1.8];
         % set(gca,'ylim',b,'yTick',b,'xLim',[0,length(X)],'xtickLabel','');
         set(gca,'ylim',b,'yTick',b,'xLim',[6,45],'xtickLabel','');
         bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
         hold off;
     case {3}
-        b=[0,0.8];
+        b=[0.8,1.8];
         set(gca,'ylim',b,'yTick',b,'xLim',[11,90],'xtickLabel','');
         bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
         hold off;
     case {5,7,9}
-          b=[0,0.8];
+        b=[0.8, 1.8];
         set(gca,'ylim',b,'yTick',b,'xLim',[6,45],'xtickLabel','');
         bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
         hold off;        
@@ -273,6 +258,101 @@ if ~isempty(SavePath)
     saveas(G,fullfile(SavePath, [vals,'_',T.String,'.eps']),'psc2')
     %     saveas(G,fullfile(SavePath, [vals,'_',T.String]),'bmp')
 end
+
+%% RD
+%
+vals ='rd';
+val_AC = rd(AMD_Ctl,:);
+val_AMD = rd(AMD,:);
+
+AMD_data  = val_AMD;
+
+%% Wilcoxon Single rank test
+% container
+group =2;
+M = length(AMD_Ctl);
+pac = nan(M,group);
+
+clear h
+
+for jj= 1: nodes
+    
+    pac(:,1)= val_AC(:,jj);
+    pac(1:8,2)= val_AMD(:,jj);
+    
+    [p(jj),h(jj),~] = signrank(pac(:,1),pac(:,2));
+    %     co = multcompare(stats(jj),'display','off');
+    %     C{jj}=co;
+end
+
+% logical 2 double
+h = h+0;
+
+%%
+G = figure; hold on;
+X = 1:nodes;
+c = lines(length(AMD));
+
+% Control
+st = nanstd(val_AC);
+m   = nanmean(val_AC,1);
+
+% render control subjects range
+A3 = area(m+2*st);
+A1 = area(m+st);
+A2 = area(m-st);
+A4 = area(m-2*st);
+
+% set color and style
+set(A1,'FaceColor',[0.6 0.6 0.6],'linestyle','none')
+set(A2,'FaceColor',[0.8 0.8 0.8],'linestyle','none')
+set(A3,'FaceColor',[0.8 0.8 0.8],'linestyle','none')
+set(A4,'FaceColor',[1 1 1],'linestyle','none')
+
+plot(m,'color',[0 0 0], 'linewidth',3 )
+
+% add individual FA plot
+for k = 1:length(AMD) %1:length(subDir)
+    plot(X,AMD_data(k,:),'Color',c(k,:),...
+        'linewidth',1);
+end
+m   = nanmean(AMD_data,1);
+plot(X,m,'Color',c(3,:) ,'linewidth',3)
+
+T = title(sprintf('%s comparing to AMD_C', fbName{fibID}(3:end)));
+ylabel(upper(vals))
+xlabel('Location')
+
+% axis
+% if b(1)<0;b(1)=0;end;
+% switch vals
+%     case {}
+switch fibID
+    case {1}
+        b=[0.8, 1.8];
+        % set(gca,'ylim',b,'yTick',b,'xLim',[0,length(X)],'xtickLabel','');
+        set(gca,'ylim',b,'yTick',b,'xLim',[6,45],'xtickLabel','');
+        bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+        hold off;
+    case {3}
+        b=[0.2,1.1];
+        set(gca,'ylim',b,'yTick',b,'xLim',[11,90],'xtickLabel','');
+        bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+        hold off;
+    case {5,7,9}
+        b=[0.2,1.1];
+        set(gca,'ylim',b,'yTick',b,'xLim',[6,45],'xtickLabel','');
+        bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+        hold off;        
+end
+clear h
+% Save current figure
+if ~isempty(SavePath)
+    saveas(G,fullfile(SavePath, [vals,'_',T.String,'.eps']),'psc2')
+    %     saveas(G,fullfile(SavePath, [vals,'_',T.String]),'bmp')
+end
+
+
 
 return
 
