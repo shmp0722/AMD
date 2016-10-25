df701e359f30b5426d93634692a387cb12d07583
shmp0722 <shmp0722@gmail.com>
2016-10-25 19:01:07 +0900

    check stats

--- DiffusionMeasure/voxelwise2/OverLapping.m
+++ DiffusionMeasure/voxelwise2/OverLapping.m
@@ -8,3 +8,7 @@ load /home/ganka/git/AMD/DiffusionMeasure/voxelwise2/T.mat
 mean(T.OR03vs1530_stdf)
 mean(T.OR1530vs3090_stdf)
 mean(T.OR03vs3090_stdf)
+
+ttest2(T.OR03vs1530_stdf(1:8),T.OR03vs1530_stdf(1:8))
+ttest2(T.OR1530vs3090_stdf(1:8),T.OR1530vs3090_stdf(1:8))
+ttest2(T.OR03vs3090_stdf(1:8),T.OR03vs3090_stdf(1:8))
