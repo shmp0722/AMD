0490fc95e69a9efeeea3a8514659cf865d1e8478
shmp0722 <shmp0722@gmail.com>
2017-01-23 20:55:53 +0900

    additional figures

--- DiffusionMeasure/Merged_AMD_plot_ttest.m
+++ DiffusionMeasure/Merged_AMD_plot_ttest.m
@@ -33,8 +33,10 @@ fa = nan(length(ACH), nodes);
 md = fa;
 ad = fa;
 rd = fa;
-
-%% get values and merge both hemisphere
+cl = fa;
+pl = fa;
+sp = fa;
+% get values and merge both hemisphere
 for subID = 1:length(ACH);
     if isempty(ACH{subID,fibID});
         fa(subID,:) =nan(1,nodes);
@@ -62,6 +64,27 @@ for subID = 1:length(ACH);
     else
         ad(subID,:) = nanmean([ ACH{subID,fibID}.vals.ad;...
             ACH{subID,fibID+1}.vals.ad]);
+    end;
+    
+    if isempty(ACH{subID,fibID});
+        cl(subID,:) =nan(1,nodes);
+    else
+        cl(subID,:) = nanmean([ ACH{subID,fibID}.vals.cl;...
+            ACH{subID,fibID+1}.vals.cl]);
+    end;
+    
+    if isempty(ACH{subID,fibID});
+        sp(subID,:) =nan(1,nodes);
+    else
+        sp(subID,:) = nanmean([ ACH{subID,fibID}.vals.sphericity;...
+            ACH{subID,fibID+1}.vals.sphericity]);
+    end;
+
+    if isempty(ACH{subID,fibID});
+        pl(subID,:) =nan(1,nodes);
+    else
+        pl(subID,:) = nanmean([ ACH{subID,fibID}.vals.planarity;...
+            ACH{subID,fibID+1}.vals.planarity]);
     end;
 end
 
@@ -418,4 +441,171 @@ end
 
 return
 
+%% cl
+%
+vals ='cl';
+val_AC = cl(AMD_Ctl,:);
+val_AMD = cl(AMD,:);
+
+AMD_data  = val_AMD;
+
+% Wilcoxon rank sum test
+% container
+clear h
+
+for jj= 1: nodes
+    [p(jj),h(jj),~] = ranksum(val_AC(:,jj),val_AMD(:,jj));
+   [H(jj),P(jj),~] = ttest2(val_AC(:,jj),val_AMD(:,jj));
+end
+
+% logical 2 double
+h = h+0;
+
+% figure 
+G = figure; hold on;
+X = 1:nodes;
+c = lines(length(AMD));
+
+bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+
+
+% Control
+st = nanstd(val_AC);
+m   = nanmean(val_AC,1);
+
+% render control subjects range
+A1 = plot(m+st,':','color',[0.6 0.6 0.6]);
+A2 = plot(m-st,':','color',[0.6 0.6 0.6]);
+A3 = plot(m+2*st,':','color',[0.8 0.8 0.8]);
+A4 = plot(m-2*st,':','color',[0.8 0.8 0.8]);
+
+plot(m,'color',[0 0 0], 'linewidth',3 )
+
+% add individual FA plot
+for k = 1:length(AMD) %1:length(subDir)
+    plot(X,AMD_data(k,:),'--r',...
+        'linewidth',1);
+end
+m   = nanmean(AMD_data,1);
+plot(X,m,'r' ,'linewidth',3)
+
+T = title(sprintf('%s comparing to AMD_C', fbName{fibID}(3:end)));
+ylabel(upper(vals))
+xlabel('Location')
+
+% axis
+% if b(1)<0;b(1)=0;end;
+% switch vals
+%     case {}
+switch fibID
+    case {1}
+        b=[0.8, 1.8];
+        % set(gca,'ylim',b,'yTick',b,'xLim',[0,length(X)],'xtickLabel','');
+        set(gca,'ylim',b,'yTick',b,'xLim',[6,45],'XTick',[6,45],'xtickLabel','');
+        bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+
+        hold off;
+    case {3}
+        b=[0.6,1.1];
+        set(gca,'ylim',b,'yTick',b,'xLim',[11,90],'XTick',[11 90],'xtickLabel','');
+        bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+        hold off;
+    case {5,7,9}
+        b=[0, .4];
+        set(gca,'ylim',b,'yTick',b,'xLim',[6,45],'XTick',[6,45],'xtickLabel','');
+%         bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+        hold off;
+end
+clear h
+% Save current figure
+if ~isempty(SavePath)
+    saveas(G,fullfile(SavePath, [vals,'_',T.String,'.eps']),'psc2')
+        saveas(G,fullfile(SavePath, [vals,'_',T.String]),'png')
+        !mv *eps DiffusivionPropertyPlot/
+        !mv *png DiffusivionPropertyPlot/
+end
+
+%% sp
+%
+vals ='sp';
+val_AC = sp(AMD_Ctl,:);
+val_AMD = sp(AMD,:);
+
+AMD_data  = val_AMD;
+
+% Wilcoxon rank sum test
+% container
+clear h
+
+for jj= 1: nodes
+    [p(jj),h(jj),~] = ranksum(val_AC(:,jj),val_AMD(:,jj));
+   [H(jj),P(jj),~] = ttest2(val_AC(:,jj),val_AMD(:,jj));
+end
+
+% logical 2 double
+h = h+0;
+
+% figure 
+G = figure; hold on;
+X = 1:nodes;
+c = lines(length(AMD));
+
+bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+
+
+% Control
+st = nanstd(val_AC);
+m   = nanmean(val_AC,1);
+
+% render control subjects range
+A1 = plot(m+st,':','color',[0.6 0.6 0.6]);
+A2 = plot(m-st,':','color',[0.6 0.6 0.6]);
+A3 = plot(m+2*st,':','color',[0.8 0.8 0.8]);
+A4 = plot(m-2*st,':','color',[0.8 0.8 0.8]);
+
+plot(m,'color',[0 0 0], 'linewidth',3 )
+
+% add individual FA plot
+for k = 1:length(AMD) %1:length(subDir)
+    plot(X,AMD_data(k,:),'--r',...
+        'linewidth',1);
+end
+m   = nanmean(AMD_data,1);
+plot(X,m,'r' ,'linewidth',3)
+
+T = title(sprintf('%s comparing to AMD_C', fbName{fibID}(3:end)));
+ylabel(upper(vals))
+xlabel('Location')
+
+% axis
+% if b(1)<0;b(1)=0;end;
+% switch vals
+%     case {}
+switch fibID
+    case {1}
+        b=[0.8, 1.8];
+        % set(gca,'ylim',b,'yTick',b,'xLim',[0,length(X)],'xtickLabel','');
+        set(gca,'ylim',b,'yTick',b,'xLim',[6,45],'XTick',[6,45],'xtickLabel','');
+        bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+
+        hold off;
+    case {3}
+        b=[0.6,1.1];
+        set(gca,'ylim',b,'yTick',b,'xLim',[11,90],'XTick',[11 90],'xtickLabel','');
+        bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+        hold off;
+    case {5,7,9}
+        b=[0.4, 1.0];
+        set(gca,'ylim',b,'yTick',b,'xLim',[6,45],'XTick',[6,45],'xtickLabel','');
+%         bar(X,h*(b(1)+0.1),1.0,'EdgeColor','none')
+        hold off;
+end
+clear h
+% Save current figure
+if ~isempty(SavePath)
+    saveas(G,fullfile(SavePath, [vals,'_',T.String,'.eps']),'psc2')
+        saveas(G,fullfile(SavePath, [vals,'_',T.String]),'png')
+        !mv *eps DiffusivionPropertyPlot/
+        !mv *png DiffusivionPropertyPlot/
+end
 
--- WhereDamaged/WhereDamaged.m
+++ WhereDamaged/WhereDamaged.m
@@ -24,23 +24,27 @@ F_fgR = fgRead(fullfile(Full_fbDir,R_FullfgName.name));
 
 DivfbDir = fullfile(homeDir,List{ii},'dwi_1st/fibers/conTrack/OR_divided');
 L_PerifbName = dir(fullfile(DivfbDir,'*Lt-LGN4_lh_Ecc30to90*Rh_NOT_MD4.pdb'));
+L_midfgName  = dir(fullfile(DivfbDir,'*Lt-LGN4_lh_Ecc15to30*Rh_NOT_MD3.pdb'));
 L_CorefbName = dir(fullfile(DivfbDir,'*Lt-LGN4_lh_Ecc0to3*Rh_NOT_MD4.pdb'));
 
 R_PerifbName = dir(fullfile(DivfbDir,'*Rt-LGN4_rh_Ecc30to90*Lh_NOT_MD4.pdb'));
+R_midfgName  = dir(fullfile(DivfbDir,'*Rt-LGN4_rh_Ecc15to30*Lh_NOT_MD3.pdb'));
 R_CorefbName = dir(fullfile(DivfbDir,'*Rt-LGN4_rh_Ecc0to3*Lh_NOT_MD4.pdb'));
 
-L_FgFull = fgRead(fullfile(Full_fbDir,L_FullfbName.name));
+L_FgFull     = fgRead(fullfile(Full_fbDir,L_FullfbName.name));
 L_FgDiv3090  = fgRead(fullfile(DivfbDir,L_PerifbName.name));
-L_FgDiv03  = fgRead(fullfile(DivfbDir,L_CorefbName.name));
+L_FgDiv03    = fgRead(fullfile(DivfbDir,L_CorefbName.name));
+L_FgMid      = fgRead(fullfile(DivfbDir,L_midfgName.name));
 
 R_FgFull = fgRead(fullfile(Full_fbDir,R_FullfgName.name));
 R_FgDiv3090  = fgRead(fullfile(DivfbDir,R_PerifbName.name));
 R_FgDiv03  = fgRead(fullfile(DivfbDir,R_CorefbName.name));
+R_FgMid      = fgRead(fullfile(DivfbDir,R_midfgName(1).name));
 
 
 %
 dt = dtiLoadDt6( fullfile(homeDir, List{ii}, '/dwi_1st/dt6.mat'));
-fg= {L_FgDiv03,R_FgDiv03,L_FgDiv3090,R_FgDiv3090};
+fg= {L_FgDiv03,R_FgDiv03,L_FgDiv3090,R_FgDiv3090,L_FgMid,R_FgMid};
 
 %%
 figure; hold on;
@@ -116,4 +120,39 @@ t1 = niftiRead(dt.files.t1);
 AFQ_AddImageTo3dPlot(t1,[0,0,-25])
 AFQ_AddImageTo3dPlot(t1,[-3,0,0])
 
-return
+%%
+figure; hold on;
+for jj = 5:6
+    vals = dtiGetValFromFibers(dt.dt6,fg{jj},inv(dt.xformToAcpc),'fa');
+    
+    %% nodes found significant difference
+    
+    % see Merged_AMD_plot_ttest
+    switch jj
+        case {1,2}
+            node_sdif = [24:37]*2;
+        case {3,4}
+            node_sdif = [12,13]*2;
+        case {5,6}   
+            node_sdif = [30,34]*2;         
+    end
+    for KK = 1:length(vals)
+        box = size(vals{KK});
+        
+        r_node = box(1);
+        node_affected = floor([r_node/100*node_sdif(1), r_node/100*node_sdif(end)]);
+        
+        vals{KK} =zeros(box);
+        vals{KK}(node_affected(1):node_affected(2))=0.3;
+    end
+    
+    rgb = vals2colormap(vals);
+    AFQ_RenderFibers(fg{jj},'color',rgb,'numfibers',100,'newfig',0);
+    
+       
+end
+axis auto
+axis off
+camlight left
+
+title 'Damaged Mid OR'
